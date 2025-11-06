eb2052872694d1c766885840a472b799965c544e
ljx <1445101363@qq.com>
2025-11-06 16:15:33 +0800

    close print

--- labelImg.py
+++ labelImg.py
@@ -2287,7 +2287,6 @@ class MainWindow(QMainWindow, WindowMixin):
         self.set_format(FORMAT_YOLO)
         tYoloParseReader = YoloReader(txtPath, self.image, self.lastOpenDir, self.labelHist)
         shapes = tYoloParseReader.getShapes()
-        print (shapes)
         self.loadLabels(shapes)
         self.canvas.verified = tYoloParseReader.verified
 
